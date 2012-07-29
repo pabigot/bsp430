813c77846f1170a981e5926163d3d8661d89521c
Peter A. Bigot <bigotp@acm.org>
2012-07-29 14:28:22 -0500

    Permit source to be compiled when uptime not enabled

--- src/utility/uptime.c
+++ src/utility/uptime.c
@@ -32,6 +32,8 @@
 #include <bsp430/utility/uptime.h>
 #include <bsp430/periph/timer_.h>
 
+#if configBSP430_UPTIME - 0
+
 void
 vBSP430uptimeStart_ni (void)
 {
@@ -54,3 +56,5 @@ vBSP430uptimeResume_ni (void)
 {
   BSP430_UPTIME_TIMER_HAL_HANDLE->timer->ctl |= MC_2;
 }
+
+#endif /* configBSP430_UPTIME */
