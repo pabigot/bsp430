97fa0d1eb7527e1bab7b7e6ba9496eb05d1d98b9
Peter A. Bigot <bigotp@acm.org>
2012-07-31 21:48:04 -0500

    Add basic resources

--- include/bsp430/platform/exp430fg4618.h
+++ include/bsp430/platform/exp430fg4618.h
@@ -44,7 +44,7 @@
  *
  * <li> #BSP430_PERIPH_EXPOSED_CLOCKS Clocks are exposed on header H2,
  * just below the FG4618 JTAG header.  MCLK is available on H2.2
- * (P1.1), SMCLK on H2.5 (P1.4), and ACLK on H2.7 (P1.5)
+ * (P1.1), SMCLK on H2.5 (P1.4), and ACLK on H2.6 (P1.5)
  *
  * </ul>
  */
@@ -52,6 +52,45 @@
 /** Unconditionally define this, so as to produce errors if there is a
  * conflict in definition. */
 #define BSP430_PLATFORM_EXP430FG4618 1
+
+/** Where clocks are found on this platform */
+#define BSP430_PERIPH_EXPOSED_CLOCKS_HELP "H2 below FG4618 JTAG: MCLK on H2.2, SMCLK on H2.5, ACLK on H2.6"
+
+/* What to use as a console */
+/* !BSP430! module=console subst=module instance=nop */
+/* !BSP430! insert=module_startif */
+/* BEGIN AUTOMATICALLY GENERATED CODE---DO NOT MODIFY [module_startif] */
+#if ((configBSP430_CONSOLE - 0)                                    \
+     && ((! defined(configBSP430_CONSOLE_USE_DEFAULT_RESOURCE))    \
+         || (configBSP430_CONSOLE_USE_DEFAULT_RESOURCE - 0)))
+/* END AUTOMATICALLY GENERATED CODE [module_startif] */
+/* !BSP430! end=module_startif */
+#define BSP430_CONSOLE_SERIAL_HAL_HANDLE xBSP430usci_USCI_A0
+/* !BSP430! insert=module_endif */
+/* BEGIN AUTOMATICALLY GENERATED CODE---DO NOT MODIFY [module_endif] */
+#endif /* configBSP430_CONSOLE && need default */
+/* END AUTOMATICALLY GENERATED CODE [module_endif] */
+/* !BSP430! end=module_endif */
+
+/* How to use ACLK as a capture/compare input source */
+/* !BSP430! module=timer feature=ccaclk subst=module,feature instance=nop */
+/* !BSP430! insert=feature_startif */
+/* BEGIN AUTOMATICALLY GENERATED CODE---DO NOT MODIFY [feature_startif] */
+#if ((configBSP430_TIMER_CCACLK - 0)                                    \
+     && ((! defined(configBSP430_TIMER_USE_DEFAULT_CCACLK_RESOURCE))    \
+         || (configBSP430_TIMER_USE_DEFAULT_CCACLK_RESOURCE - 0)))
+/* END AUTOMATICALLY GENERATED CODE [feature_startif] */
+/* !BSP430! end=feature_startif */
+#define BSP430_TIMER_CCACLK 1
+#define BSP430_TIMER_CCACLK_PERIPH_HANDLE BSP430_PERIPH_TB0
+#define BSP430_TIMER_CCACLK_IS_TA0 0
+#define BSP430_TIMER_CCACLK_CC_INDEX 6
+#define BSP430_TIMER_CCACLK_CCIS CCIS_1
+/* !BSP430! insert=feature_endif */
+/* BEGIN AUTOMATICALLY GENERATED CODE---DO NOT MODIFY [feature_endif] */
+#endif /* configBSP430_TIMER_CCACLK && need default */
+/* END AUTOMATICALLY GENERATED CODE [feature_endif] */
+/* !BSP430! end=feature_endif */
 
 /* Include generic file, in case this is being included directly */
 #include <bsp430/platform.h>
