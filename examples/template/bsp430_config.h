22a2f64c7a5ab3a82a25f585533cbe8a3442c2f4
Peter A. Bigot <bigotp@acm.org>
2012-08-05 10:39:41 -0500

    Enhance template to provide more of what is commonly needed by an app

--- examples/template/bsp430_config.h
+++ examples/template/bsp430_config.h
@@ -1,2 +1,15 @@
+/* Use a crystal if one is installed.  Much more accurate timing
+ * results. */
+#define BSP430_PLATFORM_BOOT_CONFIGURE_LFXT1 1
+
+/* Application does output: support spin-for-jumper */
+#define configBSP430_PLATFORM_SPIN_FOR_JUMPER 1
+
+/* Support console output */
+#define configBSP430_CONSOLE 1
+
+/* Monitor uptime and provide generic ACLK-driven timer */
+#define configBSP430_UPTIME 1
+
 /* Get platform defaults */
 #include <bsp430/platform/bsp430_config.h>
--- examples/template/main.c
+++ examples/template/main.c
@@ -8,8 +8,16 @@
  */
 
 #include <bsp430/platform.h>
+#include <bsp430/clock.h>
+#include <bsp430/utility/uptime.h>
+#include <bsp430/utility/console.h>
 
 void main ()
 {
   vBSP430platformInitialize_ni();
+  (void)iBSP430consoleInitialize();
+  while (1) {
+    cprintf("Up %lu ACLK ticks\n", ulBSP430uptime_ni());
+    BSP430_CORE_DELAY_CYCLES(BSP430_CLOCK_NOMINAL_MCLK_HZ);
+  }
 }
