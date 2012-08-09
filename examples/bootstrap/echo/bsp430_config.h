9e2a6b99143ef9b8bb735b6f14eb0d7a7ee3ac33
Peter A. Bigot <bigotp@acm.org>
2012-08-09 11:09:16 -0500

    Remove outdated demo program
    This mis-uses the console which should never have a transmit callback
    installed.

--- examples/bootstrap/echo/bsp430_config.h
+++
@@ -1,9 +0,0 @@
-/* Application does output: support spin-for-jumper */
-#ifndef configBSP430_PLATFORM_SPIN_FOR_JUMPER
-#define configBSP430_PLATFORM_SPIN_FOR_JUMPER 1
-#endif /* configBSP430_PLATFORM_SPIN_FOR_JUMPER */
-
-/* Request console resources */
-#define configBSP430_CONSOLE 1
-
-#include <bsp430/platform/bsp430_config.h>
