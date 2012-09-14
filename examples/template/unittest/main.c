814a6081bcf268f3e41037c1977abd8d9851ad0b
Peter A. Bigot <bigotp@acm.org>
2012-09-14 10:56:31 -0500

    Add distinct template for unittests

---
+++ examples/template/unittest/main.c
@@ -0,0 +1,19 @@
+/** This file is in the public domain.
+ *
+ * 
+ * @homepage http://github.com/pabigot/bsp430
+ *
+ */
+
+#include <bsp430/platform.h>
+#include <bsp430/clock.h>
+#include <bsp430/utility/uptime.h>
+#include <bsp430/utility/unittest.h>
+
+void main ()
+{
+  vBSP430platformInitialize_ni();
+  vBSP430unittestInitialize();
+
+  vBSP430unittestFinalize();
+}
