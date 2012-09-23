5e0ddaedb8dbd3f2c0eac2de57331525a629351e
Peter A. Bigot <pab@pabigot.com>
2012-09-22 23:14:28 -0500

    Stub command line interpreter example

---
+++ examples/utility/cli/bsp430_config.h
@@ -0,0 +1,18 @@
+/* Use a crystal if one is installed.  Much more accurate timing
+ * results. */
+#define BSP430_PLATFORM_BOOT_CONFIGURE_LFXT1 1
+
+/* Application does output: support spin-for-jumper */
+#define configBSP430_PLATFORM_SPIN_FOR_JUMPER 1
+
+/* Support console output */
+#define configBSP430_CONSOLE 1
+
+/* Enable a 16-character rx buffer for the console */
+#define BSP430_CONSOLE_RX_BUFFER_SIZE 16
+
+/* Monitor uptime and provide generic ACLK-driven timer */
+#define configBSP430_UPTIME 1
+
+/* Get platform defaults */
+#include <bsp430/platform/bsp430_config.h>
---
+++ examples/utility/cli/main.c
@@ -0,0 +1,145 @@
+/** This file is in the public domain.
+ *
+ *
+ * @homepage http://github.com/pabigot/bsp430
+ *
+ */
+
+#include <bsp430/platform.h>
+#include <bsp430/clock.h>
+#include <bsp430/utility/uptime.h>
+#include <bsp430/utility/console.h>
+#include <bsp430/utility/cli.h>
+#include <string.h>
+#include <ctype.h>
+
+#define LAST_COMMAND NULL
+
+static int
+cmd_uptime (const char * argstr)
+{
+  BSP430_CORE_INTERRUPT_STATE_T istate;
+  BSP430_CORE_SAVE_INTERRUPT_STATE(istate);
+  BSP430_CORE_DISABLE_INTERRUPT();
+  cprintf("Up %s\n", xBSP430uptimeAsText_ni(ulBSP430uptime_ni()));
+  BSP430_CORE_RESTORE_INTERRUPT_STATE(istate);
+  return 0;
+}
+static sBSP430cliCommand dcmd_uptime = {
+  .key = "uptime",
+  .help = "# Show system uptime",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_uptime
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND &dcmd_uptime
+
+static int
+cmd_help (sBSP430cliCommandLink * chain,
+          void * param,
+          const char * command,
+          size_t command_len)
+{
+  vBSP430cliConsoleDisplayHelp(chain->cmd);
+  return 0;
+}
+static sBSP430cliCommand dcmd_help = {
+  .key = "help",
+  .help = "[cmd] # Show help on cmd or all commands",
+  .next = LAST_COMMAND,
+  .handler = cmd_help
+};
+
+// expand cmd...
+// set ival|uval|lval|ulval|date|time
+// show
+// time
+// reboot
+
+
+#define KEY_BS '\b'
+#define KEY_LF '\n'
+#define KEY_CR '\r'
+#define KEY_BEL '\a'
+#define KEY_ESC '\e'
+#define KEY_FF '\f'
+#define KEY_TAB '\t'
+#define KEY_KILL_LINE 0x15
+#define KEY_KILL_WORD 0x17
+char command[30];
+
+#define FLG_NEED_PROMPT 0x01
+#define FLG_HAVE_COMMAND 0x02
+
+void main ()
+{
+  unsigned int flags = 0;
+  char * cp;
+  
+  vBSP430platformInitialize_ni();
+  (void)iBSP430consoleInitialize();
+  vBSP430cliSetDiagnosticFunction(iBSP430cliConsoleDiagnostic);
+  cprintf("And we're up and running.\n");
+  memset(command, 0, sizeof(command));
+  cp = command;
+  while (1) {
+    int c;
+    
+    if (flags & FLG_NEED_PROMPT) {
+      *cp = 0;
+      cprintf("> %s", command);
+      flags &= ~FLG_NEED_PROMPT;
+    }
+    while (0 <= ((c = cgetchar_ni()))) {
+      if (KEY_BS == c) {
+        if (cp == command) {
+          cputchar_ni(KEY_BEL);
+        } else {
+          --cp;
+          cputtext_ni("\b \b");
+        }
+      } else if (KEY_CR == c) {
+        flags |= FLG_HAVE_COMMAND;
+        break;
+      } else if (KEY_KILL_LINE == c) {
+        cprintf("\e[%uD\e[K", cp - command);
+        cp = command;
+        *cp = 0;
+      } else if (KEY_KILL_WORD == c) {
+        char * kp = cp;
+        while (--kp > command && isspace(*kp)) {
+        }
+        while (--kp > command && !isspace(*kp)) {
+        }
+        ++kp;
+        cprintf("\e[%uD\e[K", cp-kp);
+        cp = kp;
+        *cp = 0;
+      } else if (KEY_FF == c) {
+        cputchar_ni(c);
+        flags |= FLG_NEED_PROMPT;
+      } else {
+        if ((1+cp) >= (command + sizeof(command))) {
+          cputchar_ni(KEY_BEL);
+        } else {
+          *cp++ = c;
+          cputchar_ni(c);
+        }
+      }
+    }
+    if (flags & FLG_HAVE_COMMAND) {
+      cputchar_ni('\n');
+      *cp = 0;
+      iBSP430cliExecuteCommand(&dcmd_help, 0, command);
+      cp = command;
+      *cp = 0;
+      flags &= ~FLG_HAVE_COMMAND;
+      flags |= FLG_NEED_PROMPT;
+    }
+    if (flags) {
+      continue;
+    }
+    BSP430_CORE_LPM_ENTER_NI(LPM4_bits | GIE);
+  }
+}
