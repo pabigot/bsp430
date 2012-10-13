de20e80bc2bd51c89e052e6aa639c16f599eedb4
Peter A. Bigot <pab@pabigot.com>
2012-10-13 10:14:36 -0500

    alarms: refinements to example

--- examples/utility/alarm/bsp430_config.h
+++ examples/utility/alarm/bsp430_config.h
@@ -5,13 +5,14 @@
 /* Application does output: support spin-for-jumper */
 #define configBSP430_PLATFORM_SPIN_FOR_JUMPER 1
 
-/* Support console output */
+/* Support console output, and buffer it so we don't unnecessarily
+ * delay the alarm interrupts */
 #define configBSP430_CONSOLE 1
+#define BSP430_CONSOLE_TX_BUFFER_SIZE 80
 
-/* Monitor uptime and provide generic ACLK-driven timer.  Also enable
- * its CC0 HAL so we can use that for alarms. */
+/* Monitor uptime and provide generic ACLK-driven timer so we can see
+ * how long we've been running. */
 #define configBSP430_UPTIME 1
-#define configBSP430_UPTIME_USE_DEFAULT_CC0_ISR 1
 
 /* Enable an 8-character rx buffer for the console */
 #define BSP430_CONSOLE_RX_BUFFER_SIZE 8
@@ -21,6 +22,13 @@
 
 /* Enable command completion */
 #define configBSP430_CLI_COMMAND_COMPLETION 1
+
+/* We'll use CCACLK as the basis for alarms.  Also grab the HAL
+ * support so we can count overflows, and the CC0 ISR so we can put
+ * alarms on it too. */
+#define configBSP430_TIMER_CCACLK 1
+#define configBSP430_TIMER_CCACLK_USE_DEFAULT_TIMER_HAL 1
+#define configBSP430_TIMER_CCACLK_USE_DEFAULT_CC0_ISR 1
 
 /* Get platform defaults */
 #include <bsp430/platform/bsp430_config.h>
--- examples/utility/alarm/main.c
+++ examples/utility/alarm/main.c
@@ -1,5 +1,10 @@
 /** This file is in the public domain.
  *
+ * Base-2 primes for intervals: 17 31 61 127 257 509 1021
+ * Base-10 primes for intervals: 11 101 499 997 4999 10007
+ *
+ * Define APP_SOURCE_SMCLK=1 to use SMCLK as the alarm timer source;
+ * otherwise ACLK will be used.
  *
  * @homepage http://github.com/pabigot/bsp430
  *
@@ -14,6 +19,16 @@
 #include <bsp430/periph/flash.h>
 #include <string.h>
 
+#define ALARM_TIMER_PERIPH_HANDLE BSP430_TIMER_CCACLK_PERIPH_HANDLE
+#if APP_SOURCE_SMCLK - 0
+#define APP_TASSEL TASSEL_2
+#else /* APP_SOURCE_SMCLK */
+#define APP_TASSEL TASSEL_1
+#endif /* APP_SOURCE_SMCLK */
+
+static hBSP430halTIMER alarmHAL_;
+unsigned long alarm_Hz;
+
 #define MAX_TIMERS 7
 sBSP430timerAlarm alarm_data[MAX_TIMERS];
 hBSP430timerAlarm alarm[MAX_TIMERS];
@@ -21,19 +36,19 @@ size_t nTimers;
 unsigned long wakeups;
 
 enum {
-  FLG_Active = 0x01,
-  FLG_Repeating = 0x02,
-  FLG_WakeFromLPM = 0x04,
+  FLG_SkipLost = 0x01,
+  FLG_WakeFromLPM = 0x02,
 };
 
 typedef struct sAlarmStats {
   int flags;
-  size_t count;
+  unsigned long count;
   int rc;
   unsigned long interval_tck;
   unsigned long sum_late;
   unsigned long last_late;
   unsigned long max_late;
+  unsigned long lost;
 } sAlarmStats;
 
 volatile sAlarmStats alarm_stats[MAX_TIMERS];
@@ -42,17 +57,33 @@ static int
 alarmCallback_ni (hBSP430timerAlarm alarm)
 {
   volatile sAlarmStats * ap;
-  unsigned long now_utt = ulBSP430uptime_ni();
+  unsigned long now_tck = ulBSP430timerCounter_ni(alarmHAL_, NULL);
 
   ap = alarm_stats + alarm->ccidx;
   ap->count += 1;
-  ap->last_late = now_utt - alarm->setting_tck;
+  ap->last_late = now_tck - alarm->setting_tck;
   if (ap->last_late > ap->max_late) {
     ap->max_late = ap->last_late;
   }
   ap->sum_late += ap->last_late;
   if (0 < ap->interval_tck) {
-    ap->rc = iBSP430timerAlarmSet_ni(alarm, alarm->setting_tck + ap->interval_tck);
+    unsigned long setting_tck = alarm->setting_tck + ap->interval_tck;
+    unsigned int lost = 0;
+    /* Pre-check for high-speed recurring short intervals that might
+     * violate #BSP430_TIMER_ALARM_PAST_LIMIT.  This could still fail
+     * if we're so far behind that now_tck gets out of date, but the
+     * cost of updating it can also be significant. */
+    if (setting_tck <= now_tck) {
+      lost = (now_tck - setting_tck + ap->interval_tck - 1) / ap->interval_tck;
+      setting_tck += lost * ap->interval_tck;
+    }
+    ap->rc = iBSP430timerAlarmSet_ni(alarm, setting_tck);
+    while ((0 != ap->rc) && (ap->flags & FLG_SkipLost) && (1000 > lost)) {
+      ++lost;
+      setting_tck += ap->interval_tck;
+      ap->rc = iBSP430timerAlarmSet_ni(alarm, setting_tck);
+    }
+    ap->lost += lost;
   } else {
   }
   return (ap->flags & FLG_WakeFromLPM) ? BSP430_HAL_ISR_CALLBACK_EXIT_LPM : 0;
@@ -64,155 +95,6 @@ alarmCallback_ni (hBSP430timerAlarm alarm)
  * rearranged without having to change the links. */
 const sBSP430cliCommand * commandSet;
 #define LAST_COMMAND NULL
-
-static int
-cmd_uptime (const char * command)
-{
-  int ci;
-  volatile sBSP430hplTIMER * hpl = xBSP430hplLookupTIMER(BSP430_UPTIME_TIMER_PERIPH_HANDLE);
-
-  cprintf("Uptime %lu with %lu wakeups\n", ulBSP430uptime(), wakeups);
-  for (ci = 0; ci < nTimers; ++ci) {
-    cprintf("\tCC %u: CCTL %04x CCR %04x\n", ci, hpl->cctl[ci], hpl->ccr[ci]);
-  }
-  return 0;
-}
-static sBSP430cliCommand dcmd_uptime = {
-  .key = "uptime",
-  .help = "# Show uptime timer info",
-  .next = LAST_COMMAND,
-  .handler = iBSP430cliHandlerSimple,
-  .param = cmd_uptime
-};
-#undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_uptime)
-
-static int
-cmd_stats (const char * command)
-{
-  int ai;
-
-  cprintf("Alarm statistics:\n");
-  for (ai = 0; ai < nTimers; ++ai) {
-    volatile sAlarmStats * sp = alarm_stats + ai;
-    cprintf("\t%u: flg 0x%02x, cnt %u, rc %d, inter %lu, late: %lu, max %lu, ave %lu\n",
-            ai, sp->flags, sp->count, sp->rc, sp->interval_tck, sp->last_late, sp->max_late,
-            (0 == sp->count) ? 0 : (sp->sum_late / sp->count));
-  }
-  return 0;
-}
-static sBSP430cliCommand dcmd_stats = {
-  .key = "stats",
-  .help = "# Show alarm stats",
-  .next = LAST_COMMAND,
-  .handler = iBSP430cliHandlerSimple,
-  .param = cmd_stats
-};
-#undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_stats)
-
-static int
-cmd_dump (const char * command)
-{
-  int ai;
-
-  cprintf("Alarm internals:\n");
-  for (ai = 0; ai < nTimers; ++ai) {
-    hBSP430timerAlarm ap = alarm[ai];
-    cprintf("\tcc %u, flg 0x%02x, setting %lu\n",
-            ap->ccidx, ap->flags, ap->setting_tck);
-  }
-  return 0;
-}
-static sBSP430cliCommand dcmd_dump = {
-  .key = "dump",
-  .help = "# Show alarm internals",
-  .next = LAST_COMMAND,
-  .handler = iBSP430cliHandlerSimple,
-  .param = cmd_dump
-};
-#undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_dump)
-
-static int
-cmd_configure (const char * command)
-{
-  int rv;
-  unsigned int cc;
-  int enablep = 1;
-  const char * argstr = command;
-  size_t argstr_len = strlen(argstr);
-
-  argstr_len = strlen(argstr);
-  rv = iBSP430cliStoreExtractedUI(&argstr, &argstr_len, &cc);
-  if ((0 == rv) && (0 < argstr_len)) {
-    rv = iBSP430cliStoreExtractedI(&argstr, &argstr_len, &enablep);
-    if (0 != rv) {
-      cprintf("Invalid enable flag\n");
-    }
-  }
-  if (0 == rv) {
-    if ((0 <= cc) && (cc < nTimers)) {
-      if (enablep) {
-        rv = iBSP430timerAlarmEnable(alarm[cc]);
-      } else {
-        rv = iBSP430timerAlarmDisable(alarm[cc]);
-      }
-    } else {
-      cprintf("Invalid cc value %u\n", cc);
-    }
-  } else {
-    cprintf("Unable to extract cc index from '%s'\n", command);
-  }
-  return rv;
-}
-static sBSP430cliCommand dcmd_configure = {
-  .key = "configure",
-  .help = "{cc} [0|1] # enable/disable alarm cc",
-  .next = LAST_COMMAND,
-  .handler = iBSP430cliHandlerSimple,
-  .param = cmd_configure
-};
-#undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_configure)
-
-static int
-cmd_wake (const char * command)
-{
-  int rv;
-  unsigned int cc;
-  int enablep = 1;
-  const char * argstr = command;
-  size_t argstr_len = strlen(argstr);
-
-  argstr_len = strlen(argstr);
-  rv = iBSP430cliStoreExtractedUI(&argstr, &argstr_len, &cc);
-  if ((0 == rv) && (0 < argstr_len)) {
-    rv = iBSP430cliStoreExtractedI(&argstr, &argstr_len, &enablep);
-    if (0 != rv) {
-      cprintf("Invalid enable flag\n");
-    }
-  }
-  if ((0 == rv) && (0 <= cc) && (cc < nTimers)) {
-    if (enablep) {
-      alarm_stats[cc].flags |= FLG_WakeFromLPM;
-    } else {
-      alarm_stats[cc].flags &= ~FLG_WakeFromLPM;
-    }
-  } else {
-    cprintf("Bad input: %s\n", command);
-  }
-  return rv;
-}
-static sBSP430cliCommand dcmd_wake = {
-  .key = "wake",
-  .help = "{cc} [0|1] # enable/disable wake on alarm",
-  .next = LAST_COMMAND,
-  .handler = iBSP430cliHandlerSimple,
-  .param = cmd_wake
-};
-#undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_wake)
 
 static int
 cmd_cancel (const char * command)
@@ -238,7 +120,7 @@ cmd_cancel (const char * command)
   }
   return rv;
 }
-static sBSP430cliCommand dcmd_cancel = {
+static const sBSP430cliCommand dcmd_cancel = {
   .key = "cancel",
   .help = "{cc} # cancel alarm cc",
   .next = LAST_COMMAND,
@@ -269,7 +151,7 @@ cmd_alarm (const char * command)
       BSP430_CORE_SAVE_INTERRUPT_STATE(istate);
       BSP430_CORE_DISABLE_INTERRUPT();
       do {
-        abs_when = ulBSP430uptime_ni() + rel_when;
+        abs_when = ulBSP430timerCounter_ni(alarmHAL_, NULL) + rel_when;
         rv = iBSP430timerAlarmSet_ni(alarm[cc], abs_when);
       } while (0);
       BSP430_CORE_RESTORE_INTERRUPT_STATE(istate);
@@ -283,7 +165,7 @@ cmd_alarm (const char * command)
   }
   return rv;
 }
-static sBSP430cliCommand dcmd_alarm = {
+static const sBSP430cliCommand dcmd_alarm = {
   .key = "alarm",
   .help = "{cc} {rel_when} # set alarm for rel_when ticks",
   .next = LAST_COMMAND,
@@ -294,45 +176,74 @@ static sBSP430cliCommand dcmd_alarm = {
 #define LAST_COMMAND (&dcmd_alarm)
 
 static int
-cmd_reset (const char * command)
+cmd_flags (const char * command)
 {
   int rv;
   unsigned int cc;
+  const char * key;
+  int enablep = 0;
   const char * argstr = command;
   size_t argstr_len = strlen(argstr);
+  int flags = 0;
 
   argstr_len = strlen(argstr);
+  key = argstr;
   rv = iBSP430cliStoreExtractedUI(&argstr, &argstr_len, &cc);
-  if ((0 == rv) && (0 <= cc) && (cc < nTimers)) {
-    volatile sAlarmStats * sp = alarm_stats + cc;
-    BSP430_CORE_INTERRUPT_STATE_T istate;
+  if ((0 > cc) || (nTimers <= cc)) {
+    rv = -1;
+  }
+  while (0 == rv) {
+    unsigned int flag;
+    size_t key_len;
 
-    BSP430_CORE_SAVE_INTERRUPT_STATE(istate);
-    BSP430_CORE_DISABLE_INTERRUPT();
-    do {
-      sp->count = 0;
-      sp->last_late = 0;
-      sp->max_late = 0;
-      sp->sum_late = 0;
-    } while (0);
-    BSP430_CORE_RESTORE_INTERRUPT_STATE(istate);
-  } else {
-    cprintf("Bad input: %s\n", command);
+    key = xBSP430cliNextToken(&argstr, &argstr_len, &key_len);
+    if (0 == key_len) {
+      break;
+    }
+    enablep = -1;
+    if ('+' == *key) {
+      enablep = 1;
+    } else if ('-' == *key) {
+      enablep = 0;
+    } else {
+      rv = -1;
+    }
+    ++key;
+    --key_len;
+    if (0 == strncmp("wake", key, key_len)) {
+      flag = FLG_WakeFromLPM;
+    } else if (0 == strncmp("skip", key, key_len)) {
+      flag = FLG_SkipLost;
+    } else {
+      rv = -1;
+    }
+    if (0 == rv) {
+      flags |= flag;
+      if (enablep) {
+        alarm_stats[cc].flags |= flag;
+      } else {
+        alarm_stats[cc].flags &= ~flag;
+      }
+    }
+  }
+  cprintf("Successfully modified flags 0x%x\n", flags);
+  if (0 != rv) {
+    cprintf("error at %s\n", key);
   }
   return rv;
 }
-static sBSP430cliCommand dcmd_reset = {
-  .key = "reset",
-  .help = "{cc} # reset alarm statistics",
+static const sBSP430cliCommand dcmd_flags = {
+  .key = "flags",
+  .help = "{cc} {[+-]flg}... # enable/disable wake skip",
   .next = LAST_COMMAND,
   .handler = iBSP430cliHandlerSimple,
-  .param = cmd_reset
+  .param = cmd_flags
 };
 #undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_reset)
+#define LAST_COMMAND (&dcmd_flags)
 
 static int
-cmd_repeat (const char * command)
+cmd_interval (const char * command)
 {
   int rv;
   unsigned int cc;
@@ -356,15 +267,254 @@ cmd_repeat (const char * command)
   }
   return rv;
 }
-static sBSP430cliCommand dcmd_repeat = {
-  .key = "repeat",
+static const sBSP430cliCommand dcmd_interval = {
+  .key = "interval",
   .help = "{cc} {interval} # set alarm interval, 0 one-shot",
   .next = LAST_COMMAND,
   .handler = iBSP430cliHandlerSimple,
-  .param = cmd_repeat
+  .param = cmd_interval
 };
 #undef LAST_COMMAND
-#define LAST_COMMAND (&dcmd_repeat)
+#define LAST_COMMAND (&dcmd_interval)
+
+static int
+cmd_configure_ (const char * command,
+                int enablep)
+{
+  int rv;
+  unsigned int cc;
+  const char * input;
+  const char * argstr = command;
+  size_t argstr_len = strlen(argstr);
+
+  argstr_len = strlen(argstr);
+  do {
+    input = argstr;
+    rv = iBSP430cliStoreExtractedUI(&argstr, &argstr_len, &cc);
+    if ((0 == rv) && (0 <= cc) && (cc < nTimers)) {
+      if (enablep) {
+        rv = iBSP430timerAlarmEnable(alarm[cc]);
+      } else {
+        rv = iBSP430timerAlarmDisable(alarm[cc]);
+      }
+    } else {
+      rv = -1;
+    }
+  } while (0 == rv);
+  if (0 < argstr_len) {
+    cprintf("Invalid cc index at '%s'\n", input);
+    return -1;
+  }
+  return 0;
+}
+
+static int
+cmd_disable (const char * command)
+{
+  return cmd_configure_(command, 0);
+}
+static const sBSP430cliCommand dcmd_disable = {
+  .key = "disable",
+  .help = "{cc}... # disable alarm cc",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_disable
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_disable)
+
+static int
+cmd_enable (const char * command)
+{
+  return cmd_configure_(command, 1);
+}
+static const sBSP430cliCommand dcmd_enable = {
+  .key = "enable",
+  .help = "{cc}... # enable alarm cc",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_enable
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_enable)
+
+static int
+cmd_reset (const char * command)
+{
+  int rv;
+  unsigned int cc;
+  const char * argstr = command;
+  size_t argstr_len = strlen(argstr);
+  BSP430_CORE_INTERRUPT_STATE_T istate;
+
+  do {
+    rv = iBSP430cliStoreExtractedUI(&argstr, &argstr_len, &cc);
+    if ((0 == rv) && (0 <= cc) && (cc < nTimers)) {
+      volatile sAlarmStats * sp = alarm_stats + cc;
+      BSP430_CORE_SAVE_INTERRUPT_STATE(istate);
+      BSP430_CORE_DISABLE_INTERRUPT();
+      do {
+        sp->count = 0;
+        sp->last_late = 0;
+        sp->max_late = 0;
+        sp->sum_late = 0;
+        sp->lost = 0;
+      } while (0);
+      BSP430_CORE_RESTORE_INTERRUPT_STATE(istate);
+    }
+  } while (0 == rv);
+  return 0;
+}
+static const sBSP430cliCommand dcmd_reset = {
+  .key = "reset",
+  .help = "{cc}... # reset alarm statistics",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_reset
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_reset)
+
+static int
+cmd_stats (const char * command)
+{
+  int ai;
+
+  cprintf("Alarm statistics:\n");
+  for (ai = 0; ai < nTimers; ++ai) {
+    volatile sAlarmStats * sp = alarm_stats + ai;
+    cprintf(" %u: %cset %cenab %cwake %cskip cnt %lu, rc %d, inter %lu\n"
+            "    lost %lu; late: last %lu, max %lu, ave %lu\n",
+            ai,
+            (alarm[ai]->flags & BSP430_TIMER_ALARM_FLAG_SET) ? '+' : '-',
+            (alarm[ai]->flags & BSP430_TIMER_ALARM_FLAG_ENABLED) ? '+' : '-',
+            (sp->flags & FLG_WakeFromLPM) ? '+' : '-',
+            (sp->flags & FLG_SkipLost) ? '+' : '-',
+            sp->count, sp->rc, sp->interval_tck,
+            sp->lost, sp->last_late, sp->max_late,
+            (0 == sp->count) ? 0 : (sp->sum_late / sp->count));
+  }
+  return 0;
+}
+static const sBSP430cliCommand dcmd_stats = {
+  .key = "stats",
+  .help = "# Show alarm stats",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_stats
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_stats)
+
+static int
+cmd_dump (const char * command)
+{
+  unsigned long now_tck;
+  unsigned int overflows;
+  int ai;
+
+  now_tck = ulBSP430timerCounter(alarmHAL_, &overflows);
+  cprintf("Timer ticks %lu (0x%08lx) with %u overflows and %lu wakeups\n",
+          now_tck, now_tck, overflows, wakeups);
+  cprintf("Alarm internals:\n");
+  for (ai = 0; ai < nTimers; ++ai) {
+    hBSP430timerAlarm ap = alarm[ai];
+    int ci = ap->ccidx;
+    cprintf("\tcc %u, flg 0x%02x, setting %lu ; CCTL %04x CCR %04x\n",
+            ci, ap->flags, ap->setting_tck,
+            alarmHAL_->hpl->cctl[ci], alarmHAL_->hpl->ccr[ci]);
+  }
+  return 0;
+}
+static const sBSP430cliCommand dcmd_dump = {
+  .key = "dump",
+  .help = "# Show alarm internals",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_dump
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_dump)
+
+static int
+cmd_convert (sBSP430cliCommandLink * chain,
+             void * param,
+             const char * command,
+             size_t command_len)
+{
+  unsigned long when_tck;
+  int rv;
+
+  rv = iBSP430cliStoreExtractedUL(&command, &command_len, &when_tck);
+  if (0 == rv) {
+    unsigned long ms = (when_tck * 1000) / alarm_Hz;
+    unsigned long us = (when_tck * 1000) / (alarm_Hz / 1000);
+    cprintf("%lu tt at %lu Hz = %lu ms = %lu us\n", when_tck, alarm_Hz, ms, us);
+  }
+  return rv;
+}
+static const sBSP430cliCommand dcmd_convert = {
+  .key = "convert",
+  .help = "{tt} # show tt value in human time",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_convert
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_convert)
+
+static int
+cmd_clocks (const char * argstr)
+{
+  unsigned long now_tck;
+  unsigned long timer_overhead;
+
+  BSP430_CORE_INTERRUPT_STATE_T istate;
+
+  cprintf("CPU clocks (Hz): MCLK %lu ; SMCLK %lu ; ACLK %u\n",
+          ulBSP430clockMCLK_Hz(), ulBSP430clockSMCLK_Hz(),
+          uiBSP430clockACLK_Hz());
+
+  BSP430_CORE_SAVE_INTERRUPT_STATE(istate);
+  BSP430_CORE_DISABLE_INTERRUPT();
+  do {
+    unsigned long t0;
+
+    t0 = ulBSP430timerCounter_ni(alarmHAL_, NULL);
+    now_tck = ulBSP430timerCounter_ni(alarmHAL_, NULL);
+    timer_overhead = now_tck - t0;
+  } while (0);
+  BSP430_CORE_RESTORE_INTERRUPT_STATE(istate);
+  cprintf("Timer %s freq %lu Hz now %lu\n", xBSP430timerName(ALARM_TIMER_PERIPH_HANDLE), alarm_Hz, now_tck);
+  cprintf("\tResolution %lu ns/tt, overhead %lu tt @ 32bit\n",
+          1000000000UL/alarm_Hz, timer_overhead);
+  return 0;
+}
+static const sBSP430cliCommand dcmd_clocks = {
+  .key = "clocks",
+  .help = "# Clock/timer information",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_clocks
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND &dcmd_clocks
+
+static int
+cmd_uptime (const char * command)
+{
+  cprintf("Up %s\n", xBSP430uptimeAsText_ni(ulBSP430uptime()));
+  return 0;
+}
+static const sBSP430cliCommand dcmd_uptime = {
+  .key = "uptime",
+  .help = "# Show system uptime",
+  .next = LAST_COMMAND,
+  .handler = iBSP430cliHandlerSimple,
+  .param = cmd_uptime
+};
+#undef LAST_COMMAND
+#define LAST_COMMAND (&dcmd_uptime)
 
 static int
 cmd_help (sBSP430cliCommandLink * chain,
@@ -375,7 +525,7 @@ cmd_help (sBSP430cliCommandLink * chain,
   vBSP430cliConsoleDisplayHelp(chain->cmd);
   return 0;
 }
-static sBSP430cliCommand dcmd_help = {
+static const sBSP430cliCommand dcmd_help = {
   .key = "help",
   .help = "[cmd] # Show help on cmd or all commands",
   .next = LAST_COMMAND,
@@ -394,16 +544,27 @@ void main ()
   (void)iBSP430consoleInitialize();
   vBSP430cliSetDiagnosticFunction(iBSP430cliConsoleDiagnostic);
 
-  nTimers = iBSP430timerSupportedCCs(BSP430_UPTIME_TIMER_PERIPH_HANDLE);
-  cprintf("Alarm testing with timer %s having %u CCs\n",
-          xBSP430timerName(BSP430_UPTIME_TIMER_PERIPH_HANDLE), nTimers);
+  alarmHAL_ = hBSP430timerLookup(ALARM_TIMER_PERIPH_HANDLE);
+  if (! alarmHAL_) {
+    cprintf("ERROR -- Timer not available\n");
+    return;
+  }
+  vBSP430timerResetCounter_ni(alarmHAL_);
+  alarmHAL_->hpl->ctl = APP_TASSEL | MC_2 | TACLR | TAIE;
+  alarm_Hz = ulBSP430timerFrequency_Hz_ni(ALARM_TIMER_PERIPH_HANDLE);
+
+  nTimers = iBSP430timerSupportedCCs(ALARM_TIMER_PERIPH_HANDLE);
   if (nTimers > MAX_TIMERS) {
     nTimers = MAX_TIMERS;
   }
 
   for (ai = 0; ai < nTimers; ++ai) {
-    alarm[ai] = hBSP430timerAlarmInitialize(alarm_data + ai, BSP430_UPTIME_TIMER_PERIPH_HANDLE, ai, alarmCallback_ni);
+    alarm[ai] = hBSP430timerAlarmInitialize(alarm_data + ai, ALARM_TIMER_PERIPH_HANDLE, ai, alarmCallback_ni);
   }
+
+  cprintf("Alarm testing with timer %s having %u CCs\n",
+          xBSP430timerName(ALARM_TIMER_PERIPH_HANDLE), nTimers);
+  cmd_clocks("");
 
   /* NOTE: The control flow in this is a bit tricky, as we're trying
    * to leave interrupts enabled during the main body of the loop,
