cf24eec98f5ec5451c6892c2c4270d675c77bcdb
Peter A. Bigot <pab@pabigot.com>
2012-12-25 12:25:06 -0600

    tmp102: fix bogus temperature conversion

--- examples/sensors/tmp102/main.c
+++ examples/sensors/tmp102/main.c
@@ -34,12 +34,32 @@ void main ()
 
   (void)iBSP430i2cSetAddresses_ni(i2c, -1, APP_TMP102_I2C_ADDRESS);
 
-#define BSP430_TMP_xCel_TO_ddegF(xcel_) (320 + ((9 * (xcel_ >> 1)) / (4 >> (1 & (xcel_)))))
+  /** Raw number is a 16 bit value.  First 12 bits represent the
+   * temperature as a count of 0.0625C values.  (If the LSB is 1, then
+   * an extended temperature is used and the 13th bit represents a
+   * half count.)  0.625 = 5/8; shifting by 3 gets us the 13-bit
+   * value; dividing by 2 accounts for the half-count in extended
+   * temperature mode. */
+#define TMP102_RAW_TO_dC_(raw_) (5 * ((raw_) >> 3) / 16)
+#define TMP102_RAW_TO_dC(raw_) ((0 <= (int)(raw_)) ? TMP102_RAW_TO_dC_(raw_) : -TMP102_RAW_TO_dC_(-(int)(raw_)))
+#define dC_TO_dF(dC_) (320 + 9 * (dC_) / 5)
+
+#if 0
+  {
+    unsigned int data[] = { 0x7FF0, 0x4B00, 0x1900, 0xFFC0, 0xE700, 0xC908 };
+    int i;
+
+    for (i = 0; i < sizeof(data)/sizeof(*data); ++i) {
+      int temp_dC = TMP102_RAW_TO_dC(data[i]);
+      cprintf("temp 0x%04x = %d dC = %d d[degF]\n", data[i], temp_dC, dC_TO_dF(temp_dC));
+    }
+  }
+#endif
 
   while (1) {
     int rc;
     uint8_t data[2];
-    uint16_t temp_xCel;
+    uint16_t raw;
 
     rc = iBSP430i2cTxData_ni(i2c, &pr, 1);
     if (0 > rc) {
@@ -52,11 +72,12 @@ void main ()
       cprintf("I2C RX ERROR\n");
       continue;
     }
-    temp_xCel = data[1] | (data[0] << 8);
+    raw = data[1] | (data[0] << 8);
     if (0 == pr) {
-      cprintf("temp 0x%04x = %d d[degF]\n", temp_xCel, BSP430_TMP_xCel_TO_ddegF(temp_xCel));
+      int temp_dC = TMP102_RAW_TO_dC(raw);
+      cprintf("temp 0x%04x = %d dC = %d d[degF]\n", raw, temp_dC, dC_TO_dF(temp_dC));
     } else {
-      cprintf("reg %d is 0x%04x\n", pr, temp_xCel);
+      cprintf("reg %d is 0x%04x\n", pr, raw);
     }
     pr = (pr + 1) & 0x03;
     BSP430_CORE_DELAY_CYCLES(BSP430_CLOCK_NOMINAL_MCLK_HZ);
