170fb3dff3fe841b0284f7943a52f1d1c054fb66
Peter A. Bigot <pab@pabigot.com>
2012-12-28 16:56:14 -0600

    ds18b20: fix issues with measurement times

--- examples/sensors/ds18b20/main.c
+++ examples/sensors/ds18b20/main.c
@@ -46,7 +46,6 @@ struct sBSP430onewireSerialNumber serial;
 void main ()
 {
   int rc;
-  unsigned long uptime_ticks_per_sec;
   const struct sBSP430onewireBus * bus = &ds18b20;
 
   vBSP430platformInitialize_ni();
@@ -54,9 +53,8 @@ void main ()
   (void)iBSP430consoleInitialize();
   cprintf("\nHere we go...\n");
 
-  uptime_ticks_per_sec = ulBSP430uptimeConversionFrequency_Hz_ni();
   cprintf("Uptime now %lu with frequency %lu Hz\n",
-          ulBSP430uptime_ni(), uptime_ticks_per_sec);
+          ulBSP430uptime_ni(), ulBSP430uptimeConversionFrequency_Hz_ni());
 
   cprintf("Monitoring DS18xx on %s.%u bit %x\n",
           xBSP430portName(BSP430_PORT_HAL_GET_PERIPH_HANDLE(APP_DS18B20_PORT_HAL)) ?: "P?",
@@ -79,32 +77,37 @@ void main ()
     unsigned long start_tck;
     unsigned long end_tck;
     unsigned int duration_ms;
-    unsigned int delay_count = 0;
     int t_c;
 
     start_tck = ulBSP430uptime_ni();
     rc = -1;
     if (0 == iBSP430onewireRequestTemperature_ni(bus)) {
-      /* Wait for read to complete.  Each read is nominally 61
-       * microseconds.  Conversion time can be as long as 750 ms if
-       * 12-bit resolution is used.  (12-bit resolution is the
-       * default.) */
+      /* Wait for read to complete.  Conversion time can be as long as
+       * 750 ms if 12-bit resolution is used (this resolution is the
+       * default). Timing will be wrong unless interrupts are enabled
+       * so uptime overflow events can be handled.  Sleep for 600ms,
+       * then test at 10ms intervals until the result is ready.  Each
+       * check is nominally 61 microseconds. */
+      BSP430_UPTIME_DELAY_MS_NI(600, LPM4_bits, 0);
       while (! iBSP430onewireReadBit_ni(bus)) {
-        ++delay_count;
+        BSP430_UPTIME_DELAY_MS_NI(10, LPM4_bits, 0);
       }
       rc = iBSP430onewireReadTemperature_ni(bus, &t_c);
     }
     end_tck = ulBSP430uptime_ni();
-    duration_ms = 1000 * (end_tck - start_tck) / uptime_ticks_per_sec;
+    duration_ms = BSP430_UPTIME_UTT_TO_MS(end_tck - start_tck);
 
+    cprintf("%s: ", xBSP430uptimeAsText_ni(end_tck));
     if (0 == rc) {
       cprintf("Temperature %d dCel or %d d[degF] in %u ms\n",
-              (10 * t_c) / 16, BSP430_ONEWIRE_xCel_TO_ddegF(t_c), duration_ms);
+              (10 * t_c) / 16, BSP430_ONEWIRE_xCel_TO_ddegF(t_c),
+              duration_ms);
     } else {
       cprintf("Measurement failed in %u ms\n", duration_ms);
     }
 
     /* You'd want to do this if you were going to sleep here */
     vBSP430onewireShutdown_ni(bus);
+    BSP430_UPTIME_DELAY_MS_NI(5000, LPM4_bits, 0);
   }
 }
