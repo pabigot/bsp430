85ae24f993a2eafe4fd9d2546eeeba9aa9d93550
Peter A. Bigot <pab@pabigot.com>
2013-03-25 11:56:21 -0500

    Update for 20130325 release

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-Release: 20130110
+Release: 20130325
 
 BSP430 is a set of C language adaptation layers simplifying cross-MCU
 library and application development on Texas Instruments MSP430
--- include/bsp430/core.h
+++ include/bsp430/core.h
@@ -56,7 +56,7 @@
  * the BSP430 library that is being used.  The value represents a
  * development freeze date in the form YYYYMMDD as a decimal
  * number. */
-#define BSP430_VERSION 20130110
+#define BSP430_VERSION 20130325
 
 /** Constant to specify no peripheral in preprocessor checks.
  *
